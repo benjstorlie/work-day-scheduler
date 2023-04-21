6b24c8525cc11c2189597c6fba5a63274e927376
bjs <127002178+benstorlie@users.noreply.github.com>
2023-04-20 22:24:44 -0700

    added comments to javascript

--- script.js
+++ script.js
@@ -5,47 +5,21 @@ let currentDay = dayjs();
 let currentHour = currentDay.hour(); // currentHour type is Number.
 
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
 
-// **Does this actually call the function as it is defined?
 $(function () {
 
-
+  // Define the DOM elements as JQuery objects
   const currentDayEl = $("#currentDay");
   const mainEl = $("#main");
 
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
-  // **Do a for-loop to create the elements #hour-08 to #hour-17
+  // Do a for-loop to create the elements #hour-08 to #hour-17
   for (let h=startHour; h<endHour; h++) {
     //add to main element addTimeBlock(h);
     mainEl.append(addTimeBlock(h));
   }
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
+  // Display the current day.  currentDay is defined as dayjs() at the top
   currentDayEl.text(currentDay.format("dddd, MMMM D"))
 
 });
@@ -56,32 +30,41 @@ function addTimeBlock(h) {
   // create day.js object for hour h.
   let hour = dayjs().hour(h);
 
-  let timeBlock = $('<div>');
-  timeBlock.addClass("row time-block");
-  timeBlock.attr("id","hour-"+h);
+  let timeBlock = $('<div>')
+    .addClass("row time-block")
+    .attr("data-hour",h)
+    .attr("id","hour-"+h);
 
+  // Add appropriate class to timeBlock depending on whether time h is in the past, present, or future.
+  // currentHour is defined as dayjs().hour() at the top.
   if (h == currentHour) {
     timeBlock.addClass("present");
   } else if (h < currentHour) {
     timeBlock.addClass("past");
   } else if (h > currentHour) {
     timeBlock.addClass("future");
-  } else {console.log("Error. h="+ h + ", currentHour="+currentHour)}
+  } else {
+    // This should only happen if h or currentHour are not Number types, which shouldn't happen.
+    console.log("Error. h=" + h + ", currentHour="+currentHour)
+  }
 
-  let hourEl = $('<div>');
-  hourEl.addClass("col-2 col-md-1 hour text-center py-3");
-  hourEl.text(hour.format("hA")); 
+  // Define and append to the time block the other various html elements
+  let hourEl = $('<div>')
+    .addClass("col-2 col-md-1 hour text-center py-3")
+    .text(hour.format("hA"));  // This displays the time formatted like 9AM or 4PM
   hourEl.appendTo(timeBlock); 
 
-  let description = $('<textarea>');
-  description.addClass("col-8 col-md-10 description");
-  description.attr("rows","3");
+  // This is the part you write in
+  let description = $('<textarea>')
+    .addClass("col-8 col-md-10 description")
+    .attr("rows","3");
   description.appendTo(timeBlock);
 
-  let saveBtn = $("<button>");
-  saveBtn.addClass("btn saveBtn col-2 col-md-1")
-  saveBtn.attr("aria-label","save");
-  saveBtn.html("<i class='fas fa-save' aria-hidden='true'></i>");
+  // This is the save button
+  let saveBtn = $("<button>")
+    .addClass("btn saveBtn col-2 col-md-1")
+    .attr("aria-label","save")
+    .html("<i class='fas fa-save' aria-hidden='true'></i>");
   saveBtn.appendTo(timeBlock);
 
   let storedText = localStorage.getItem(h)
@@ -97,5 +80,3 @@ function addTimeBlock(h) {
 
   return timeBlock;
 }
-
-// function saveEvent()
